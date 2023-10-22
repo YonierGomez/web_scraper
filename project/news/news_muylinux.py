b7508e05a1040ca55af66c9e979bdb7170978668
Yonier Gómez <yasprill@bancolombia.com.co>
2023-10-22 00:06:48 -0500

    Se añaden buenas prácticas a muylinux

--- project/news/news_muylinux.py
+++ project/news/news_muylinux.py
@@ -1,10 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 def news(URL):
     """Funcion para obtener noticias de muy linux"""
-    r = requests.get(URL)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    
+    r = requests.get(URL, headers=headers)
 
     get_news = []
     if r.ok:
@@ -14,8 +17,6 @@ def news(URL):
         soup = BeautifulSoup(leer, 'html.parser')
         title = soup.find('div', class_='zoxrel zox100')
         
-
-            
         for r_title in title.find_all('div', class_='zox-art-title', limit=10):
             get_news.append(f'*{r_title.a.h2.text}: {r_title.a["href"]}')
         
